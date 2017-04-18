7cedad1b66adc79eaad46c1abdcbaff2d08d9fd7
MrZebarth <kris.zebarth@ocsb.ca>
2017-04-18 13:00:29 -0400

    Added Project

---
+++ src/Run.java
@@ -0,0 +1,19 @@
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * You are to create a Caesar Cipher program
+		 * Read in a single letter or number indicating how much to shift by
+		 * Read in a message
+		 * Output the converted message.
+		 * Give the user an option to decode the message as well.
+		 * Remember:
+		 * String line="Message";
+		 * line=line.toUpperCase(); //line="MESSAGE"
+		 * char letter=line.charAt(0); //letter='M'
+		 */
+	}
+
+}
