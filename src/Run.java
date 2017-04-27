42672af8b9cdadcea1ba0dcdfc641ab43ad02111
nickgg11 <nicholas.gingras@ocsbstudent.ca>
2017-04-27 13:16:50 -0400

    Added short program

--- src/Run.java
+++ src/Run.java
@@ -28,273 +28,47 @@ public class Run {
 		words=input.split("");
 		if (type==2)
 		{
+			System.out.println("Okay what letter do you want to shift to?");
+			String input2=in.nextLine();
+			char shift=input2.charAt(0);
+
 			for (int x=0;x<words.length;x++)
-			{
-				String currentWord=words[x];
-				if (currentWord.charAt(0)=='a')
+			{			
+				char letter=input.charAt(x);
+				if (letter>='a'&&letter<='z')
 				{
-					currentWord="x";
-				}
-				else if (currentWord.charAt(0)=='b')
-				{
-					currentWord="y";
-				}
-				else if (currentWord.charAt(0)=='z')
-				{
-					currentWord="w";
-				}
+					letter+=shift-'a';
+					if (letter>'z')
+					{
+						letter-=26;
+					}
 
-				else if (currentWord.charAt(0)=='c')
-				{
-					currentWord="z";
 				}
-
-				else if (currentWord.charAt(0)=='d')
-				{
-					currentWord="a";
-				}
-
-				else if (currentWord.charAt(0)=='e')
-				{
-					currentWord="b";
-				}
-
-				else if (currentWord.charAt(0)=='f')
-				{
-					currentWord="c";
-				}
-
-				else if (currentWord.charAt(0)=='g')
-				{
-					currentWord="d";
-				}
-
-				else if (currentWord.charAt(0)=='h')
-				{
-					currentWord="e";
-				}
-
-				else if (currentWord.charAt(0)=='i')
-				{
-					currentWord="f";
-				}
-
-				else if (currentWord.charAt(0)=='j')
-				{
-					currentWord="g";
-				}
-
-				else if (currentWord.charAt(0)=='k')
-				{
-					currentWord="h";
-				}
-
-				else if (currentWord.charAt(0)=='l')
-				{
-					currentWord="i";
-				}
-
-				else if (currentWord.charAt(0)=='m')
-				{
-					currentWord="j";
-				}
-
-				else if (currentWord.charAt(0)=='n')
-				{
-					currentWord="k";
-				}
-
-				else if (currentWord.charAt(0)=='o')
-				{
-					currentWord="l";
-				}
-
-				else if (currentWord.charAt(0)=='p')
-				{
-					currentWord="m";
-				}
-
-				else if (currentWord.charAt(0)=='q')
-				{
-					currentWord="n";
-				}
-
-				else if (currentWord.charAt(0)=='r')
-				{
-					currentWord="o";
-				}
-
-				else if (currentWord.charAt(0)=='s')
-				{
-					currentWord="p";
-				}
-
-				else if (currentWord.charAt(0)=='t')
-				{
-					currentWord="q";
-				}
-
-				else if (currentWord.charAt(0)=='u')
-				{
-					currentWord="r";
-				}
-
-				else if (currentWord.charAt(0)=='v')
-				{
-					currentWord="s";
-				}
-				else if (currentWord.charAt(0)=='w')
-				{
-					currentWord="t";
-				}
-
-				else if (currentWord.charAt(0)=='x')
-				{
-					currentWord="u";
-				}
-
-				else if (currentWord.charAt(0)=='y')
-				{
-					currentWord="v";
-				}
-				output=output+currentWord;
+				output+=letter;
 			}
 			System.out.println(output);
 		}
-		else 
+		else
 		{
+			System.out.println("Okay what letter did you shift by?");
+			String input2=in.nextLine();
+			char shift=input2.charAt(0);
+
 			for (int x=0;x<words.length;x++)
 			{
-				String currentWord=words[x];
-				if (currentWord.charAt(0)=='x')
+				char letter=input.charAt(x);
+				if (letter>='a'&&letter<='z')
 				{
-					currentWord="a";
+					letter+='a'-shift;
+					if (letter<'a')
+					{
+						letter+=26;
+					}
 				}
-				else if (currentWord.charAt(0)=='y')
-				{
-					currentWord="b";
-				}
-				else if (currentWord.charAt(0)=='w')
-				{
-					currentWord="z";
-				}
-
-				else if (currentWord.charAt(0)=='z')
-				{
-					currentWord="c";
-				}
-
-				else if (currentWord.charAt(0)=='a')
-				{
-					currentWord="d";
-				}
-
-				else if (currentWord.charAt(0)=='b')
-				{
-					currentWord="e";
-				}
-
-				else if (currentWord.charAt(0)=='c')
-				{
-					currentWord="f";
-				}
-
-				else if (currentWord.charAt(0)=='d')
-				{
-					currentWord="g";
-				}
-
-				else if (currentWord.charAt(0)=='e')
-				{
-					currentWord="h";
-				}
-
-				else if (currentWord.charAt(0)=='f')
-				{
-					currentWord="i";
-				}
-
-				else if (currentWord.charAt(0)=='g')
-				{
-					currentWord="j";
-				}
-
-				else if (currentWord.charAt(0)=='h')
-				{
-					currentWord="k";
-				}
-
-				else if (currentWord.charAt(0)=='i')
-				{
-					currentWord="l";
-				}
-
-				else if (currentWord.charAt(0)=='j')
-				{
-					currentWord="m";
-				}
-
-				else if (currentWord.charAt(0)=='k')
-				{
-					currentWord="n";
-				}
-
-				else if (currentWord.charAt(0)=='l')
-				{
-					currentWord="o";
-				}
-
-				else if (currentWord.charAt(0)=='m')
-				{
-					currentWord="p";
-				}
-
-				else if (currentWord.charAt(0)=='n')
-				{
-					currentWord="q";
-				}
-
-				else if (currentWord.charAt(0)=='o')
-				{
-					currentWord="r";
-				}
-
-				else if (currentWord.charAt(0)=='p')
-				{
-					currentWord="s";
-				}
-
-				else if (currentWord.charAt(0)=='q')
-				{
-					currentWord="t";
-				}
-
-				else if (currentWord.charAt(0)=='r')
-				{
-					currentWord="u";
-				}
-
-				else if (currentWord.charAt(0)=='s')
-				{
-					currentWord="v";
-				}
-				else if (currentWord.charAt(0)=='t')
-				{
-					currentWord="w";
-				}
-
-				else if (currentWord.charAt(0)=='u')
-				{
-					currentWord="x";
-				}
-
-				else if (currentWord.charAt(0)=='v')
-				{
-					currentWord="y";
-				}
-				output=output+currentWord;
+				output+=letter;
 			}
 			System.out.println(output);
 		}
+
 	}
 }
